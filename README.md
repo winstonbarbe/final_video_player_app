4269a9c8275397e057c338ffe6d60183c63fff7c
winstonbarbe <winston.barbe@gmail.com>
2021-05-02 21:00:45 -0400

    adds some functionality with new styling

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+**Update**
+
+As I restyle the page with mobile use in mind I find myself repeating some mistakes. Yes, CSS is frustatin, but yes, I will get it done.
+
+- Style more with conditional logic for session dependent aspects.
+
 1. Get flex nav menu to work using dropwdown or some animation.
 2. Add main body, video and library and upload;
 3. Finsih login and session info
--- new-scripts/main.js
+++ new-scripts/main.js
@@ -1,10 +1,73 @@
+// Nav Stuff
+
+const showNoSessions = document.querySelectorAll(".show-no-sessions");
+const menuToggle = document.getElementById("menu-toggle");
+const nav = document.querySelector(".nav");
+const logout = document.querySelector(".logout");
+
+
+function handleMenuToggle() {
+  nav.style.flexWrap === "wrap" ? nav.style.flexWrap = "" :  nav.style.flexWrap = "wrap";
+  showNoSessions.forEach(controller => {
+    controller.style.display === "flex" ?  controller.style.display = "none" : controller.style.display = "flex";
+  });
+}
+menuToggle.addEventListener("click", handleMenuToggle);
+
+// Signup Stuff
 const signupReveal = document.querySelector("#signup-reveal");
 const signupForm = document.getElementById("signup-form");
-const cancelButtons = document.querySelectorAll(".cancelbtn");
-console.log(cancelButtons);
+const signupCancelButton = document.querySelector(".signup-form .cancelbtn");
 
 function handleSignupReveal (e) {
+  signupForm.classList.toggle("not-clicked");
+  loginForm.classList.add("not-clicked");
+}
+function handleSignup(e) {
+  e.preventDefault();
+  console.log(e);
+  this.reset();
   signupForm.classList.toggle("not-clicked");
 }
 
 signupReveal.addEventListener("click", handleSignupReveal);
+signupCancelButton.addEventListener("click", handleSignupReveal);
+signupForm.addEventListener("submit", handleSignup);
+
+// Login Stuff
+const loginReveal = document.querySelector("#login-reveal");
+const loginForm = document.getElementById("login-form");
+const loginCancelButton = document.querySelector(".login-form .cancelbtn");
+
+function handleLoginReveal (e) {
+  loginForm.classList.toggle("not-clicked");
+  signupForm.classList.add("not-clicked");
+}
+function handleLogin(e) {
+  e.preventDefault();
+  console.log(e);
+  localStorage.setItem("jwt", "jwt");
+  this.reset();
+  loginForm.classList.toggle("not-clicked");
+  showNoSessions.forEach(controller => {
+    controller.classList.replace("show-no-session", "hide-on-session");
+  });
+  logout.classList.replace("hide-no-session", "show-on-session");
+  console.log(logout);
+}
+
+loginReveal.addEventListener("click", handleLoginReveal);
+loginCancelButton.addEventListener("click", handleLoginReveal);
+loginForm.addEventListener("submit", handleLogin);
+
+// Logout
+
+function handleLogout() {
+  showNoSessions.forEach(controller => {
+    logout.classList.add("logout-no-session");
+    controller.classList.replace("inactive", "session-controller");
+  });
+  localStorage.clear();
+}
+logout.addEventListener("click", handleLogout);
+
